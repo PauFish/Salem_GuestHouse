902ba5f7e3daf1558cf833d4ee40325973072e7f
Pau Siquier <pausiqrey8@gmail.com>
2022-07-11 21:23:12 +0200

    Initial commit

---
+++ aboutus.php
@@ -0,0 +1,162 @@
+<h1>About Us</h1>
+  <div class="intro">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Intro Content -->
+        <div class="col-lg-6">
+          <div class="intro_content">
+            <div class="section_title">
+              <div>Welcome</div>
+              <h1>Amazing Guest house near the Sea</h1>
+            </div>
+            <div class="intro_text">
+              <p>Hi I am Salem,a local fisherman born and raised in Ras al Hadd, my family and I have been welcoming visitors to the areas for over 15 years. It will be my pleasure to welcome you here and show you true Omani hospitality and lifestyle, and the wonders of Ras al Hadd; truly charming and unique in beauty and nature.</p>
+              <p></p>
+              <p>The guesthouse is a humble venture put together by locals and supported friends of Ras al Hadd, we have 5 rooms available and are just minutes away from the beach! We will also advise you on which beaches to visit, for sunset walks, day trips or a morning swim. Book a boat trip for fishing, dolphin-watching and snorkeling; directly from us, places guaranteed! Catch of the day BBQ also available for an unforgettable dinner Bedouin-style, with local fresh fish. I also do guided desert tours 4x4, camp, accommodation and food included just message me for more information and availability.  I hope you will make many memories here and we look forward to welcoming you soon!</p>
+            </div>
+        <!-- Intro Image -->
+        <div class="col-lg-6">
+          <div class="intro_image">
+            <div class="background_image" style="background-image:url(images/intro.jpg)"></div>
+            <img src="images/intro.jpg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+        
+  <div class="intro">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Intro Content -->
+        <div class="col-lg-6">
+          <div class="intro_content">
+            <div class="section_title">
+             <h1>Ras al Hadd</h1>
+            </div>
+            <div class="intro_text">
+              <p>Ras al Hadd is a small fishing village, with basic amenities. Petrol Station, pharmacy, supermarket, small shops and restaurants serving simple indian-style food. There is NO ATM machine in Ras al Hadd, the nearest ATM in in Sur. Locals are friendly and approachable.</p>
+              <p></p>
+              <p>Getting around? Car is best, its also possible to walk to some places. Parking is available on the property. There is no public transport in Ras al Hadd. Taxi's and buses run from Sur to Muscat, the bus service is called MWAsalat.</p>
+            </div>
+        <!-- Intro Image -->
+        <div class="col-lg-6">
+          <div class="intro_image">
+            <div class="background_image" style="background-image:url(images/intro2.jpeg)"></div>
+            <img src="images/intro2.jpeg" alt="">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Offering -->
+
+  <div class="offering">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>SalemFish</div>
+            <h1>What we offer</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row offering_row">
+        
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_1.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Guest House</h3></div>
+              <div class="offer_text">
+                <p>We recently completely renovated our Guest House during 2021/2022 to offer you the best possible service. Everything is new and ready. book now!</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_2.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Turtle</h3></div>
+              <div class="offer_text">
+                <p>Ras al Hadd it is the best enclave to see green turtles. You can see them walking along the beach, and dont' miss baby turtles hatching out and running to the sea almost all year round!</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_3.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Dolphin Tour</h3></div>
+              <div class="offer_text">
+                <p>Dozens of dolphins will follow the boat jumping. Our clients say that it is the best experience of their trip in Oman and one of the best of their life.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_4.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Fishing</h3></div>
+              <div class="offer_text">
+              <p>It does not matter if you are an expert Angler or want to fish for the first time. You will fish Omani style and enjoy a BBQ of your catch of the day at night.</p>
+               </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_5.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Omani BBQ for dinner</h3></div>
+              <div class="offer_text">
+              <p>We prepare it Omani style! Salem will go to the market to look for Lobsters, Tuna, Lamb... All very fresh and delicious.</p><p> If you have any intolerance or you are Vegan/Vegetarian, let us know in advance.</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+        <!-- Offer Item -->
+        <div class="col-xl-4 col-md-6">
+          <div class="offer">
+            <div class="offer_image"><img src="images/offer_6.jpg" alt=""></div>
+            <div class="offer_content text-center">
+              <div class="offer_title"><h3>Desert</h3></div>
+              <div class="offer_text">
+                <p>You will sleep in the desert enjoying millions of stars. we take care of everything, rooms, campfire, dinner, so you have a real experience in an Omani desert. Please book in advance</p>
+              </div>
+            <!--   <div class="offer_button"><a href="#">discover</a></div> -->
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>      
+
+    
+
+  
---
+++ home.php
@@ -0,0 +1,352 @@
+<div class="intro">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Welcome</div>
+            <h1>Amazing Hotel near the Sea</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row intro_row">
+        <div class="col-xl-8 col-lg-10 offset-xl-2 offset-lg-1">
+          <div class="intro_text text-center">
+            <p>You will stay at our beautiful guesthouse! We have 5 rooms With queen sized beds for a relaxing sleep after a day filled with adventures! </p> 
+            <p>All rooms are air-conditioned, en-suite private bathroom, with toilet, shower and hot water.</p>
+          </div>
+        </div>
+      </div>
+      <div class="row gallery_row">
+        <div class="col">
+
+          <!-- Gallery -->
+          <div class="gallery_slider_container">
+            <div class="owl-carousel owl-theme gallery_slider">
+              
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_1.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_2.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_3.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div class="gallery_slide">
+                <img src="images/gallery_4.jpg" alt="">
+                <div class="gallery_overlay">
+                  <div class="text-center d-flex flex-column align-items-center justify-content-center">
+                    <a href="#">
+                      <!-- <span>+</span>
+                      <span>See More</span> -->
+                    </a>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_right container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6 order-xl-1 order-2">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_1.jpg)"></div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+        <!-- Rooms Content -->
+        <div class="col-xl-6 order-xl-2 order-1">
+          <div class="rooms_right_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Room 4</h1>
+            </div>
+            <div class="rooms_text">
+              <p>Your room has 1 queen sized bed for a relaxing sleep after a day filled with adventures! The room is air-conditioned and is also equipped with ceiling fan. En-suite private bathroom, with toilet, shower and hot water.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>En-suite private bathroom</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Air-conditioned</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Shower and hot water</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">$54/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Rooms -->
+
+  <div class="rooms_left container_wrapper">
+    <div class="container">
+      <div class="row row-eq-height">
+        
+        <!-- Rooms Content -->
+        <div class="col-xl-6">
+          <div class="rooms_left_content">
+            <div class="section_title">
+              <div>Rooms</div>
+              <h1>Room 5 </h1>
+            </div>
+            <div class="rooms_text">
+              <p>Your room has 2 queen sized beds for a relaxing sleep after a day filled with adventures! The room is air-conditioned and is also equipped with ceiling fan. En-suite private bathroom, with toilet, shower and hot water.</p>
+            </div>
+            <div class="rooms_list">
+              <ul>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>En-suite private bathroom</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Air-conditioned</span>
+                </li>
+                <li class="d-flex flex-row align-items-center justify-content-start">
+                  <img src="images/check.png" alt="">
+                  <span>Shower and hot water</span>
+                </li>
+              </ul>
+            </div>
+            <div class="rooms_price">$69/<span>Night</span></div>
+           
+          </div>
+        </div>
+
+        <!-- Rooms Image -->
+        <div class="col-xl-6">
+          <div class="rooms_slider_container">
+            <div class="owl-carousel owl-theme rooms_slider">
+              
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+
+              <!-- Slide -->
+              <div class="slide">
+                <div class="background_image" style="background-image:url(images/rooms_2.jpg)"></div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+  <!-- Discover -->
+
+  <div class="discover">
+
+    <!-- Discover Content -->
+    <div class="discover_content">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5">
+            <div class="section_title">
+              <div>SalemFish</div>
+              <h1>Hi I am Salem,</h1>
+            </div>
+          </div>
+        </div>
+        <div class="row discover_row">
+          <div class="col-lg-5">
+            <div class="discover_highlight">
+              <p>A local fisherman born and raised in Ras al Hadd, my family and I have been welcoming visitors to the areas for over 15 years.</p> 
+              <p>It will be my pleasure to welcome you here and show you true Omani hospitality and lifestyle, and the wonders of Ras al Hadd; truly charming and unique in beauty and nature.</p>
+            </div>
+            <!-- <div class="button discover_button"><a href="#">discover</a></div> -->
+          </div>
+          <div class="col-lg-7">
+            <div class="discover_text">
+              <p>The guesthouse is a humble venture put together by locals and supported friends of Ras al Hadd, we have 5 rooms available and are just minutes away from the beach! </p>
+              <p>We will also advise you on which beaches to visit, for sunset walks, day trips or a morning swim. Book a boat trip for fishing, dolphin-watching and snorkeling; directly from us, places guaranteed! </p>
+              <p> Catch of the day BBQ also available for an unforgettable dinner Bedouin-style, with local fresh fish. I also do guided desert tours 4x4, camp, accommodation and food included just message me for more information and availability. </p>
+              <p>There are very few locally owned businesses in Ras al Hadd so we thank you in advance for supporting the local community! I hope you will make many memories here and we look forward to welcoming you soon! </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Discover Slider -->
+    <div class="discover_slider_container">
+      <div class="owl-carousel owl-theme discover_slider">
+        
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_1.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Tours</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_2.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Fishing</a></h1>
+          </div>
+        </div>
+
+        <!-- Slide -->
+        <div class="slide">
+          <div class="background_image" style="background-image:url(images/discover_3.jpg)"></div>
+          <div class="discover_overlay d-flex flex-column align-items-center justify-content-center">
+            <h1><a href="#">Dolphin</a></h1>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Testimonials -->
+
+  <div class="testimonials">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="section_title text-center">
+            <div>Our guests say </div>
+            <h1>Testimonials</h1>
+          </div>
+        </div>
+      </div>
+      <div class="row testimonials_row">
+        <div class="col">
+          
+          <!-- Testimonials Slider -->
+          <div class="testimonials_slider_container">
+            <div class="owl-carousel owl-theme testimonials_slider">
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Really nice house. Everything looks new. Salem is really nice, he brought us to the beach, to see the turtles and even on a boat for fishing, to see dolphins and to swim with sea turtles. We didn't expect anything for our trip to Ras Al Hadd and the experiences we had are some of our highlights of our trip to Oman. Highly recommended if you go to the region for turtles !</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_2.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Mich, </a><span> Quebec City, Canada. Jun 2022</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Our stays at Salem guest house was amazing. The place is really nice and near the sea. Salem offers a lot of activities as fishing tour and fish bbq. He was very welcoming.</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_1.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Louise, </a><span>Antony,France. June 2022</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>Our stay with Salem was the highlight of our time in Oman. We arrived to a 'catch of the day' bbq of Tuna then were lucky enough to see a turtle on the beach afterwards. After a fantastic breakfast in the morning Salem took us out in his boat to see hundreds of dolphins. Can highly recommend staying with Salem!</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_3.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Pau, </a><span> Mallorca, Spain. March 2022</span></div>
+                </div>
+              </div>
+
+              <!-- Slide -->
+              <div>
+                <div class="testimonial_text text-center">
+                  <p>We had a wonderful time staying at Salem's.A great way to experience a real Omani fishing village, it's people & the surroundings. Salem's home was clean , homely & comfortable. We loved the trip out to see the dolphins & see others fishing. Early morning walks aling the almost deserted beaches was magical. Thankyou for the Omani experience</p>
+                </div>
+                <div class="testimonial_author text-center">
+                  <div class="testimonial_author_image"><img src="images/author_4.jpg" alt=""></div>
+                  <div class="testimonial_author_name"><a href="#">Sharleen, </a><span> Sydney, Australia. March 2022</span></div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
